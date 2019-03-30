bfca7f13e79b278e9f01c726b6d5df6002887210
GaneshMD,Ruthwik <Ruthwik.GaneshMD@cerner.com>
2019-03-30 13:18:19 +0530

    devices

---
+++ Device.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+
+public class Device 
+{
+    private string Switch = "off";
+    private string deviceName;
+
+    public Device(string deviceName){
+        this.deviceName=deviceName;
+    }
+
+    public void setSwitchON()
+    {
+        this.Switch = "ON";
+    }
+
+    public void setSwitchOFF()
+    {
+        this.Switch = "OFF";
+    }
+
+    public string getSwitch()
+    {
+        return this.Switch;
+    }
+
+    public string getName()
+    {
+        return this.deviceName;
+    }
+}
---
+++ arButton.cs
@@ -0,0 +1,31 @@
+﻿using Firebase;
+using Firebase.Database;
+using Firebase.Unity.Editor;
+using UnityEngine;
+
+public class arButton : MonoBehaviour
+{
+    private Device s;
+    private DatabaseReference dbReference;
+
+    public void Start()
+    {
+        s = new Device("Appliance-1");
+        FirebaseApp.DefaultInstance.SetEditorDatabaseUrl("https://ariot-5cdcc.firebaseio.com/");
+        dbReference = FirebaseDatabase.DefaultInstance.RootReference;
+    }
+
+    public void onButton()
+    {
+        s.setSwitchON();
+        dbReference.Child(s.getName()).SetValueAsync(s.getSwitch());
+    }
+
+    public void offButton()
+    {
+        s.setSwitchOFF();
+        dbReference.Child(s.getName()).SetValueAsync(s.getSwitch());
+
+    }
+
+}
